70904e66bcf27f91af28b466dd969a518a1bb29a
Racareanu Dragos-Ionut <dragos01ionut@gmail.com>
2022-05-05 03:05:28 +0300

    GY-69 Retrieve the gym classes from the table and implement the add class action

--- src/main/java/org/loose/fis/sre/services/ClassService.java
+++ src/main/java/org/loose/fis/sre/services/ClassService.java
@@ -2,6 +2,9 @@ package org.loose.fis.sre.services;
 import java.util.*;
 import java.sql.*;
 
+import org.loose.fis.sre.exceptions.Classdoesnotexist;
+import org.loose.fis.sre.exceptions.DayandHourTaken;
+import org.loose.fis.sre.exceptions.Classwiththatscheduledoesnotexist;
 import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
 import org.loose.fis.sre.model.Class;
 import org.loose.fis.sre.DatabaseConnection;
@@ -29,7 +32,9 @@ public class ClassService {
             e.printStackTrace();
         }
     }
+
     static ResultSet queryOutput, resultSet;
+
     static {
         try {
             queryOutput = statement.executeQuery(connectQuery);
@@ -38,4 +43,49 @@ public class ClassService {
         }
     }
 
+    public static void addClass(String type, String day, String hour, int numberofclients) throws DayandHourTaken, SQLException {
+        Class gymclass = new Class(type, day, hour, numberofclients);
+        checkDayandHourTaken(gymclass);
+        connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+        preparedStatement = connection.prepareStatement(INSERT_QUERY);
+        preparedStatement.setString(1, type);
+        preparedStatement.setString(2, day);
+        preparedStatement.setString(3, hour);
+        preparedStatement.setInt(4, numberofclients);
+
+        System.out.println(preparedStatement);
+        preparedStatement.executeUpdate();
+    }
+
+    public static ArrayList<Class> getAllClasses() {
+        try {
+            ArrayList<Class> gymclasses = new ArrayList<>();
+            statement.execute("SELECT * FROM classes");
+            ResultSet resultSet = statement.getResultSet();
+
+            while (resultSet.next())
+                gymclasses.add(new Class(resultSet.getString("type"), resultSet.getString("day"),
+                        resultSet.getString("hour"), resultSet.getInt("numberofclients")));
+
+            return gymclasses.size() == 0 ? null : gymclasses;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void checkDayandHourTaken(Class gymclass) throws DayandHourTaken {
+        ArrayList<Class> gymclasses = getAllClasses();
+
+        if (gymclasses != null) {
+            Iterator<Class> it = gymclasses.iterator();
+            while (it.hasNext()) {
+                Class aux = it.next();
+                if (aux.gethour().equals(gymclass.gethour()) && aux.getday().equals(gymclass.getday()))
+                    throw new DayandHourTaken(gymclass.getday(), gymclass.gethour());
+            }
+        }
+
+    }
 }
+
