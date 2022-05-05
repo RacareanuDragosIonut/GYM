61a56b5430be86206f209b68d6cf77433c65744d
anisia1204 <anisia.rosu@student.upt.ro>
2022-05-06 01:31:57 +0300

    GY-4 Created a table with predefined gym classes and retrieved them from it

--- src/main/java/org/loose/fis/sre/controllers/MakeAnAppointmentController.java
+++ src/main/java/org/loose/fis/sre/controllers/MakeAnAppointmentController.java
@@ -6,7 +6,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
+import org.loose.fis.sre.DatabaseConnection;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.net.URL;
 
@@ -25,13 +30,33 @@ public class MakeAnAppointmentController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        classesList.getItems().addAll(classes);
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        String connectQuery = "SELECT * FROM gym_classes";
+
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryOutput = statement.executeQuery(connectQuery);
+
+            while(queryOutput.next()){
+                String gym_class = queryOutput.getString("gym_class_name");
+                String hour = queryOutput.getString("beggining_hour");
+                String day = queryOutput.getString("day");
+                String listOut = gym_class + ": " + day + " la " + hour;
+                classesList.getItems().add(listOut);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        /*classesList.getItems().addAll(classes);
         classesList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
                 hours.setText(classesList.getSelectionModel().getSelectedItem());
             }
-        });
+        });*/
 
     }
 
