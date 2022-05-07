b3c47ee21ad52d15b8d381d2c306fabe3f2aae77
anisia1204 <anisia.rosu@student.upt.ro>
2022-05-07 17:03:15 +0300

    GY-2 Created a GUI with a list view and linked it to the Home Page(Main Menu Client Tab)

---
+++ src/main/java/org/loose/fis/sre/controllers/RequestTrainerController.java
@@ -0,0 +1,24 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class RequestTrainerController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Label requestMessage;
+
+    @FXML
+    private Button sendRequestButton;
+
+    @FXML
+    private ListView<?> trainersList;
+
+
+
+}
