5628643a9d693ca7945b1c8a93d37de2250bed06
anisia1204 <anisia.rosu@student.upt.ro>
2022-05-08 01:56:01 +0300

    GY-2 Created a popup window to display details of the trainers

--- src/main/java/org/loose/fis/sre/controllers/RequestTrainerController.java
+++ src/main/java/org/loose/fis/sre/controllers/RequestTrainerController.java
@@ -1,11 +1,15 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import org.loose.fis.sre.DatabaseConnection;
+import org.loose.fis.sre.model.Trainer;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -27,8 +31,11 @@ public class RequestTrainerController implements Initializable {
     @FXML
     private ListView<String> trainersList;
 
+    @FXML
+    private Label trainerDetails;
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL arg0, ResourceBundle arg1){
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getConnection();
 
@@ -39,12 +46,30 @@ public class RequestTrainerController implements Initializable {
             ResultSet queryOutput = statement.executeQuery(connectQuery);
 
             while (queryOutput.next()) {
-                String fullname = queryOutput.getString("fullname");
-                //String type_of_training = queryOutput.getString("type_of_training");
-                //String price = queryOutput.getString("price");
-                String listOut = fullname; //+ ": " + day + " la ora " + hour;
+                String listOut = queryOutput.getString("fullname");
                 trainersList.getItems().add(listOut);
             }
+            queryOutput = statement.executeQuery(connectQuery);
+            ResultSet finalQueryOutput = queryOutput;
+            trainersList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+                    try {
+                        while (finalQueryOutput.next()) {
+                            String fullname = finalQueryOutput.getString("fullname");
+                            String type_of_training = finalQueryOutput.getString("type_of_training");
+                            String price = finalQueryOutput.getString("price");
+                            Trainer trainer = new Trainer(fullname, type_of_training, price);
+                            if (trainer.getFullname().equals(trainersList.getSelectionModel().getSelectedItem())) {
+                                String details = "Type of training: " + type_of_training + "\nPrice: " + price;
+                                trainerDetails.setText(details);
+                            }
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         } catch (SQLException e) {
             e.printStackTrace();
         }
