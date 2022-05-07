b792c6c2fd971fe6ff8ae384975ca146dbe5d395
anisia1204 <anisia.rosu@student.upt.ro>
2022-05-07 17:32:23 +0300

    GY-2 Created a table with predefined trainers and retrieved the trainers from the file

--- src/main/java/org/loose/fis/sre/controllers/RequestTrainerController.java
+++ src/main/java/org/loose/fis/sre/controllers/RequestTrainerController.java
@@ -1,11 +1,19 @@
 package org.loose.fis.sre.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import org.loose.fis.sre.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.net.URL;
 
-public class RequestTrainerController {
+public class RequestTrainerController implements Initializable {
 
     @FXML
     private Button backButton;
@@ -17,8 +25,28 @@ public class RequestTrainerController {
     private Button sendRequestButton;
 
     @FXML
-    private ListView<?> trainersList;
+    private ListView<String> trainersList;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
 
+        String connectQuery = "SELECT * FROM trainers";
 
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet queryOutput = statement.executeQuery(connectQuery);
+
+            while (queryOutput.next()) {
+                String fullname = queryOutput.getString("fullname");
+                //String type_of_training = queryOutput.getString("type_of_training");
+                //String price = queryOutput.getString("price");
+                String listOut = fullname; //+ ": " + day + " la ora " + hour;
+                trainersList.getItems().add(listOut);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
