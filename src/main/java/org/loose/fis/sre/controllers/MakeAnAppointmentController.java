94e71a361ccccebc094fb92006d5955f5458f74f
anisia1204 <anisia.rosu@student.upt.ro>
2022-05-09 19:58:37 +0300

    GY-4 Added back button

--- src/main/java/org/loose/fis/sre/controllers/MakeAnAppointmentController.java
+++ src/main/java/org/loose/fis/sre/controllers/MakeAnAppointmentController.java
@@ -3,10 +3,12 @@ package org.loose.fis.sre.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 import org.loose.fis.sre.DatabaseConnection;
 import org.loose.fis.sre.exceptions.ClassFull;
 import org.loose.fis.sre.services.GymClassesService;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -24,6 +26,9 @@ public class MakeAnAppointmentController implements Initializable {
 
     @FXML
     private Button makeanappointmentButton;
+
+    @FXML
+    private Button backButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -65,6 +70,11 @@ public class MakeAnAppointmentController implements Initializable {
             hours.setText(e.getMessage());
         }
 
+    }
+
+    public void backButtonOnAction() throws IOException {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        stage.close();
     }
 }
 
