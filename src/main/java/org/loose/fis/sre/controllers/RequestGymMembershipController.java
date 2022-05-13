6b493cab7d954b3d62eebf10ddd38d007976972d
anisia1204 <anisia.rosu@student.upt.ro>
2022-05-14 01:40:36 +0300

    GY-3 Created a popup window to display details of the list item

--- src/main/java/org/loose/fis/sre/controllers/RequestGymMembershipController.java
+++ src/main/java/org/loose/fis/sre/controllers/RequestGymMembershipController.java
@@ -1,10 +1,15 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import org.loose.fis.sre.DatabaseConnection;
+import org.loose.fis.sre.model.GymMembership;
+import org.loose.fis.sre.model.Trainer;
 
 import java.net.URL;
 import java.sql.Connection;
@@ -15,14 +20,22 @@ import java.util.ResourceBundle;
 
 public class RequestGymMembershipController implements Initializable {
 
+    GymMembership membership;
+
     @FXML
     private Button backButton;
 
     @FXML
-    private Button sendRequestButton;
+    private Label membershipDetails;
 
     @FXML
     private ListView<String> membershipsList;
+
+    @FXML
+    private Label requestMessage;
+
+    @FXML
+    private Button sendRequestButton;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -39,6 +52,29 @@ public class RequestGymMembershipController implements Initializable {
                 String listOut = queryOutput.getString("membership_type");
                 membershipsList.getItems().add(listOut);
             }
+
+            queryOutput = statement.executeQuery(connectQuery);
+            ResultSet finalQueryOutput = queryOutput;
+            membershipsList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+                    try {
+                        while (finalQueryOutput.next()) {
+                            String membership_type = finalQueryOutput.getString("membership_type");
+                            String hours = finalQueryOutput.getString("hours");
+                            String price = finalQueryOutput.getString("price");
+                            membership = new GymMembership(membership_type, hours, price);
+                            if (membership.getMembership_type().equals(membershipsList.getSelectionModel().getSelectedItem())) {
+                                String details = hours + "\nPrice: " + price;
+                                membershipDetails.setText(details);
+                                break;
+                            }
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         } catch (SQLException e) {
             e.printStackTrace();
         }
