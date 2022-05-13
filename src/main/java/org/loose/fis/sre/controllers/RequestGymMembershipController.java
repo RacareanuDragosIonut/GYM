393dce5ef313459d840d18d383e5c60d554c9ef5
anisia1204 <anisia.rosu@student.upt.ro>
2022-05-13 20:19:21 +0300

    GY-3 Created a GUI with a list view and linked it to the Home Page(Main Menu Client Tab)

---
+++ src/main/java/org/loose/fis/sre/controllers/RequestGymMembershipController.java
@@ -0,0 +1,14 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RequestGymMembershipController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button sendRequestButton;
+
+}
