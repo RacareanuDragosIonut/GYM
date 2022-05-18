a1797d5e11961cbd024894939d926c36a8c67005
anisia1204 <anisia.rosu@student.upt.ro>
2022-05-18 21:25:37 +0300

    GY-81 Added JUnit tests for MainMenuClientController

---
+++ src/test/java/org/loose/fis/sre/controllers/MainMenuClientControllerTest.java
@@ -0,0 +1,79 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.loose.fis.sre.DatabaseConnection;
+import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
+import org.loose.fis.sre.services.UserService;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@ExtendWith(ApplicationExtension.class)
+public class MainMenuClientControllerTest {
+    public static final String USERNAME = "user";
+    public static final String PASSWORD = "password";
+    public static final String FIRSTNAME = "password";
+    public static final String LASTNAME = "password";
+    public static final String AGE = "password";
+    public static final String PHONENUMBER = "password";
+    public static final String ROLE = "Client";
+
+    @BeforeEach
+    void setUp() throws SQLException, UsernameAlreadyExistsException {
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connection = connectNow.getConnection();
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/gymapp", "root", "AnisiaRosu12");
+
+        PreparedStatement preparedStatement2 = connection.prepareStatement("DELETE FROM user_account");
+        System.out.println(preparedStatement2);
+        preparedStatement2.executeUpdate();
+
+        UserService.addUser(USERNAME, PASSWORD, FIRSTNAME, LASTNAME, AGE, PHONENUMBER, ROLE);
+        PreparedStatement preparedStatement1 = connection.prepareStatement("SELECT * FROM user_account");
+        System.out.println(preparedStatement1);
+        preparedStatement1.executeQuery();
+    }
+
+    @Start
+    void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 520, 400));
+        primaryStage.show();
+    }
+
+    @Test
+    public void testMenu(FxRobot robot){
+        robot.clickOn("#usernameField");
+        robot.write(USERNAME);
+        robot.clickOn("#passwordField");
+        robot.write(PASSWORD);
+        robot.clickOn("#role");
+        robot.clickOn(ROLE);
+        robot.clickOn("#loginButton");
+
+        robot.clickOn("#makeanappointmentButton");
+        robot.clickOn("#backButton");
+
+        robot.clickOn("#requestTrainerButton");
+        robot.clickOn("#backButton");
+
+        robot.clickOn("#requestMembershipButton");
+        robot.clickOn("#backButton");
+
+        robot.clickOn("#viewTrainersHistoryButton");
+        robot.clickOn("#backButton");
+    }
+
+}
